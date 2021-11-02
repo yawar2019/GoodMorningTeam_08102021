5047e0fbf6b1db71d4a774daeb7949e63f96df88
yawar2019 <yawarali17@gmail.com>
2021-11-02 06:47:27 +0530

    committed

--- AdoDotnetExample/Controllers/DefaultController.cs
+++ AdoDotnetExample/Controllers/DefaultController.cs
@@ -37,5 +37,45 @@ namespace AdoDotnetExample.Controllers
 
             return View();
         }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            EmployeeModel emp = db.GetEmployeeById(id);
+            return View(emp);
+        }
+
+        [HttpPost]
+       
+        public ActionResult Edit(EmployeeModel emp)
+        {
+
+            int i = db.UpdateEmployee(emp);
+            if (i > 0)
+            {
+                return RedirectToAction("index");
+            }
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            EmployeeModel emp = db.GetEmployeeById(id);
+            return View(emp);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int? id)
+        {
+            int i = db.DeleteEmployee(id);
+            if (i > 0)
+            {
+                return RedirectToAction("index");
+            }
+
+            return View();
+        }
     }
 }
--- AdoDotnetExample/Models/EmployeeModel.cs
+++ AdoDotnetExample/Models/EmployeeModel.cs
@@ -17,7 +17,8 @@ namespace AdoDotnetExample.Models
     public class EmployeeContext
     {
         SqlConnection con = new SqlConnection(@"Data Source=AZAM-PC\SQLEXPRESS;Initial Catalog=Employee;Integrated Security=true");//User Id=;Password
-        public List<EmployeeModel> GetEmployee() {
+        public List<EmployeeModel> GetEmployee()
+        {
 
             List<EmployeeModel> listObj = new List<EmployeeModel>();
 
@@ -33,7 +34,7 @@ namespace AdoDotnetExample.Models
             foreach (DataRow dr in dt.Rows)
             {
                 EmployeeModel emp = new EmployeeModel();
-                emp.EmpId =Convert.ToInt32(dr[0]);
+                emp.EmpId = Convert.ToInt32(dr[0]);
                 emp.EmpName = Convert.ToString(dr[1]);
                 emp.EmpSalary = Convert.ToInt32(dr[2]);
                 listObj.Add(emp);
@@ -56,6 +57,63 @@ namespace AdoDotnetExample.Models
             return result;
 
 
+        }
+
+        public EmployeeModel GetEmployeeById(int? id)
+        {
+
+            EmployeeModel emp = new EmployeeModel();
+
+            SqlCommand cmd = new SqlCommand("sp_getNeerjaEmployeeDetailsById", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@EmpId", id);
+
+
+            DataTable dt = new DataTable();
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+            da.Fill(dt);
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                emp.EmpId = Convert.ToInt32(dr[0]);
+                emp.EmpName = Convert.ToString(dr[1]);
+                emp.EmpSalary = Convert.ToInt32(dr[2]);
+
+            }
+            return emp;
+
+        }
+
+        public int UpdateEmployee(EmployeeModel Emp)
+        {
+            SqlCommand cmd = new SqlCommand("sp_UpdateNeerjaEmployees", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            con.Open();
+            cmd.Parameters.AddWithValue("@EmpId", Emp.EmpId);
+            cmd.Parameters.AddWithValue("@EmpName", Emp.EmpName);
+            cmd.Parameters.AddWithValue("@EmpSalary", Emp.EmpSalary);
+
+            int result = cmd.ExecuteNonQuery();
+
+
+            con.Close();
+            return result;
+
+
+        }
+
+
+        public int DeleteEmployee(int? id)
+        {
+            SqlCommand cmd = new SqlCommand("sp_DeleteNeerjaEmployees", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            con.Open();
+            cmd.Parameters.AddWithValue("@EmpId", id);
+            int result = cmd.ExecuteNonQuery();
+            con.Close();
+            return result;
         }
     }
 }
