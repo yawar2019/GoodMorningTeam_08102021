52d7d1389138b9837f1c8c7cfcbf9376f35f7f28
yawar2019 <yawarali17@gmail.com>
2021-11-15 07:01:46 +0530

    commit

---
+++ GoodMorningTeam_08102021/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GoodMorningTeam_08102021.Models;
+using System.Web.Security;
+
+namespace GoodMorningTeam_08102021.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        EmployeeEntities1 db = new EmployeeEntities1();
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(UserDetail user)
+        {
+            var loginUser = db.UserDetails.Where(s => s.UserName == user.UserName && s.Password == user.Password).SingleOrDefault();
+            if(loginUser!=null)
+            {
+                FormsAuthentication.SetAuthCookie(user.UserName, false);
+                return RedirectToAction("Dashboard");
+            }
+            return View();
+        }
+        [Authorize]
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
